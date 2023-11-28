d09eaa7ce9d9d20842a4e0d505407920bf11223c
vanisimov4 <144102084+vanisimov4@users.noreply.github.com>
2023-11-28 16:23:41 +0200

    Done

--- js/task-3.js
+++ js/task-3.js
@@ -2,8 +2,8 @@
 function filterArray(numbers, value) { 
   const newArray = [];
   for (const number of numbers) { 
-    if () { 
-      
+    if (number > value) { 
+      newArray.push(number);
     }
   }
   return newArray;
@@ -15,16 +15,3 @@ console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
 console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
 console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
 console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
-
-// function checkForSpam(message) {
-//   const normalizedMessage = message.toLowerCase();
-//   return normalizedMessage.includes('spam') || normalizedMessage.includes('sale');
-// }
-
-// console.log(checkForSpam('Latest technology news')); // false
-// console.log(checkForSpam('JavaScript weekly newsletter')); // false
-// console.log(checkForSpam('Get best sale offers now!')); // true
-// console.log(checkForSpam('Amazing SalE, only tonight!')); // true
-// console.log(checkForSpam('Trust me, this is not a spam message')); // true
-// console.log(checkForSpam('Get rid of sPaM emails. Our book in on sale!')); // true
-// console.log(checkForSpam('[SPAM] How to earn fast money?')); // true
